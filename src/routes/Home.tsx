c8fdbba4c44b71ea99e3da9d1eac96723eb72400
Jiseok Yong <pqmnvv4@gmail.com>
2022-12-27 15:27:55 +0900

    second

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -6,23 +6,21 @@ import vertexShader from '../glsl/vertex';
 import fragmentShader from '../glsl/fragment';
 import * as THREE from 'three';
 import { ShaderMaterial, TextureLoader } from 'three';
-
+import image from '../../public/back1.png'
 
 
 
 
 
 export default function Home() {
-  const w = window.innerWidth;
-  const h= window.innerHeight;
 
   const Back=()=>{
     const mesh =useRef<ShaderMaterial | null>(null);
-    
+    const background = useLoader(TextureLoader,image)
     const uniforms=useMemo(
       ()=>({
       uTexture:{
-        value:new TextureLoader().load('../../public/back1.png')
+        value:background
       },
       uTime:{
         value:0.0
@@ -32,17 +30,21 @@ export default function Home() {
 
     useFrame((state)=>{
       const {clock} =state;
+      if(!mesh.current){
+        return;
+      }
       mesh.current.uniforms.uTime.value = state.clock.getElapsedTime();
     });
 
     return(
 
       <mesh
-      ref={mesh}
+      
       position={[0,0,0]}
       >
         <planeGeometry args={[1,1,32,32]}/>
         <shaderMaterial 
+        ref={mesh}
         fragmentShader={fragmentShader}
         vertexShader={vertexShader}
         uniforms={uniforms}
