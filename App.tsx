b22e6ce1e6fb15d9a925f607d63e8d0e636e30ca
Jiseok Yong <pqmnvv4@gmail.com>
2023-02-13 13:37:54 +0900

    Updates

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { BrowserRouter , Route,Routes } from "react-router-dom";
+
+
+import Home from "./routes/Home";
+
+
+
+function App() {
+ 
+  return (
+    
+    <div className="App">
+      
+      <BrowserRouter>
+      
+        <Routes>
+        <Route path="/" element={<Home />}/>
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+
+export default App;
