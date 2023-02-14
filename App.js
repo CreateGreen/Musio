e3903e3050ac1f9b6d5c2230ae7873a12422a0e4
Jiseok Yong <pqmnvv4@gmail.com>
2023-02-14 13:39:34 +0900

    Updates

--- App.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./routes/Home";
-function App() {
-    return (_jsx("div", { className: "App", children: _jsx(BrowserRouter, { children: _jsx(Routes, { children: _jsx(Route, { path: "/", element: _jsx(Home, {}) }) }) }) }));
-}
-export default App;
-//# sourceMappingURL=App.js.map
--- components/Hometext.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import './Hometext.css';
-const Hometext = () => {
-    return (_jsx("div", { className: "hometext", children: _jsxs("svg", { className: "hometext_svg", viewBox: '0 0 600 400', children: [_jsx("text", { alignmentBaseline: 'middle', x: "0", y: "50%", children: "M" }), _jsx("text", { alignmentBaseline: 'middle', x: "150", y: "50%", children: "U" }), _jsx("text", { alignmentBaseline: 'middle', x: "300", y: "50%", children: "S" }), _jsx("text", { alignmentBaseline: 'middle', x: "400", y: "50%", children: "I" }), _jsx("text", { alignmentBaseline: 'middle', x: "500", y: "50%", children: "O" })] }) }));
-};
-export default Hometext;
-//# sourceMappingURL=Hometext.js.map
