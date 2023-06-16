6fe596590a25bb4eb333a517718dda9b26c67f7d
Jiseok Yong <pqmnvv4@gmail.com>
2023-06-16 22:38:09 +0900

    create demopage image animation and text floating animation

--- src/routes/Detail.tsx
+++ src/routes/Detail.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import "./Detail.css";
 import Amazon from "../components/project/Amazon";
+import Demo from "../components/project/Demo";
 import { motion } from "framer-motion";
 
 function Detail() {
@@ -9,14 +10,17 @@ function Detail() {
     switch (data) {
       case "img2":
         return <Amazon />;
+      case "img1":
+        return <Demo url={urldata.url}/>;
     }
   };
   return (
     <motion.div
       className="detail"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 , }}
+      animate={{ opacity: 1 , }}
       exit={{ opacity: 0 }}
+      transition={{ duration:1.3}}
     >
       <div className="detail_img">
         <img src={process.env.PUBLIC_URL + `/${urldata.url}.jpg`} />
