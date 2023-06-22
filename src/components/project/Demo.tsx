5e7dbabbf3b62957ffe67fec9ef21d945aa6faa6
Jiseok Yong <pqmnvv4@gmail.com>
2023-06-22 22:57:44 +0900

    fix:demopage active image position

--- src/components/project/Demo.tsx
+++ src/components/project/Demo.tsx
@@ -13,11 +13,12 @@ function Demo({ url }: PropsType) {
   return (
     <div className="demo">
       <div className={"demo_img"+(click ? "_active":"")}>
-        <img onClick={()=>handleClick()} src={process.env.PUBLIC_URL + `/${url}.jpg`} />
+        <img onClick={()=>handleClick()} src={process.env.PUBLIC_URL + `/${url}.jpg`} alt="바다유화그림"/>
         <p className="previewtext">See Detail</p>
       </div>
       <div className="demo_text">
-
+        <p className="left">Foggy Seas Painting <br/>Ann Marie Coolick</p>
+        <p className="right">Oil on Canvas Heavily <br/>textured seascape inspired by<br/> a foggy morning on a choppy open ocean. <br/>Edges painted and wired.</p>
       </div>
     </div>
   );
