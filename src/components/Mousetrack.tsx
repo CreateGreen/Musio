34e7643c5d3bebc9c4f2e9491b9724f1cc986bcc
Jiseok Yong <pqmnvv4@gmail.com>
2023-07-12 23:45:08 +0900

    feat(Home):add mousetrack and motiondiv without rerendering

--- src/components/Mousetrack.tsx
+++ src/components/Mousetrack.tsx
@@ -1,40 +1,56 @@
-import { useFrame } from '@react-three/fiber';
+import useMouse from "@react-hook/mouse-position";
 
 
-export const Mousetrack =()=>{
-    
-    useFrame(({mouse})=>{
-        const {} = mouse;
+export const Mousetrack =(ref:React.MutableRefObject<null>)=>{
 
+    const mouse = useMouse(ref,{
+        enterDelay:70,
+        leaveDelay:200,
     })
 
     
-        let mouseY=0;
-        let mouseX =0;
-        
+    let mouseY=0;
+    let mouseX =0;
+    if(mouse.clientX!=null){
+        mouseX=mouse.clientX;
+    }
+    if(mouse.clientY!=null){
+        mouseY=mouse.clientY;
+    }
 
     return{
         default:{
             opacity:1,
-            height:10,
-            width:10,
+            height:25,
+            width:25,
             fontsize:"20px",
             backgroundColor:"#212129",
             x:mouseX,
             y:mouseY,
             transition:{
                 type:"spring",
-                mass:0.6,
-            },
+                mass:0.5,
+            },   
         },
-
-
+        change:{
+            opacity:1,
+            height:25,
+            width:25,
+            fontsize:"20px",
+            backgroundColor:"#ffffff",
+            x:mouseX,
+            y:mouseY,
+            transition:{
+                type:"spring",
+                mass:0.5,
+            },   
+        },
     }
 };
 
 export const Spring={
     type:"spring",
     stiffness:500,
-    damping:28,
+    damping:10,
 }
 
